c89f4c6c7c2b7516fab5551bd0b8dbf81acdbd62
mutaz55 <premiergroup22@gmail.com>
2020-07-25 17:07:04 +0200

    Adding a simple Authentication process

---
+++ data_entry_tool/auth.js
@@ -0,0 +1,44 @@
+
+// listen for auth changes
+
+auth.onAuthStateChanged( user => {
+    if (user) {
+        setupUI(user);
+    }else {
+        setupUI();
+    }
+});
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e)=> {
+    e.preventDefault();
+
+
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email,password). then ( cred => {
+
+      
+
+        // close the modal form and reset it
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+        loginForm.querySelector('.error').innerHTML = '';
+
+    }).catch (error => {
+        loginForm.querySelector('.error').innerHTML = error.message;
+    })
+});
+
+
+
+// logout method
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut();
+});
---
+++ data_entry_tool/index.js
@@ -0,0 +1,23 @@
+
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+const mainMsg = document.querySelector('#mainMsg');
+
+const setupUI = (user) => {
+    if (user) {
+        loggedInLinks.forEach (item => item.style.display = "block");
+        loggedOutLinks.forEach(item => item.style.display = "none");
+        mainMsg.style.display = "none";
+    }else {
+        loggedInLinks.forEach (item => item.style.display = "none");
+        loggedOutLinks.forEach(item => item.style.display = "block");
+        mainMsg.style.display = "block";
+    }
+}
+// setup materialize components
+document.addEventListener("DOMContentLoaded", function() {
+
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+
+});
