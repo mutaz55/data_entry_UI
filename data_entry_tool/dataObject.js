23cc1a98ed85042e515e8f0f97946e81735c65d4
mutaz55 <premiergroup22@gmail.com>
2020-07-29 18:07:21 +0200

    Reading from firestore 2

--- data_entry_tool/dataObject.js
+++ data_entry_tool/dataObject.js
@@ -1,5 +1,5 @@
 
-class course {
+class Course {
     constructor (id, CourseTitle,Description,  Category, ModulesNumbers) {
         this.id = id;
         this.CourseTitle = CourseTitle;
@@ -13,10 +13,77 @@ class course {
     }
 }
 
-function storeDataLocally(id, data) {
+class Module {
+    constructor(id, ModuleID, ModuleTitle) {
+        this.id = id;
+        this.ModuleID = ModuleID;
+        this.ModuleTitle = ModuleTitle;
+    }
+    toString() {
+        return this.id + ', ' + this.ModuleID + ', ' + this.ModuleTitle;
+    }
+}
+
+class Concept {
+    constructor(id, ConceptID, ConceptText){
+        this.id = id;
+        this.ConceptID = ConceptID;
+        this.ConceptText = ConceptText;
+    }
+    toString() {
+        return this.id + ', ' + this.ConceptID + ', ' + this.ConceptText;
+    }
+}
+
+class Skill {
+    constructor(id, SkillID, SkillText ) {
+        this.id = id;
+        this.SkillID = SkillID;
+        this.SkillText = SkillText;
+    }
+    toString(){
+        return this.id + ', ' + this.SkillID + ', ' + this.SkillText;
+    }
+}
+
+class Lesson {
+    constructor(id, LessonID, LessonTitle ) {
+        this.id = id;
+        this.LessonID = LessonID;
+        this.LessonTitle = LessonTitle;
+    }
+    toString() {
+        return this.id + ', ' + this.LessonID + ', ' + this.LessonTitle;
+    }
+}
+
+class SceneType {
+    constructor(id, SceneID, SceneDesc) {
+        this.id = id;
+        this.SceneID = SceneID;
+        this.SceneDesc;
+    }
+    toString() {
+        return this.id + ', ' + this.SceneID + ', ' + this.SceneDesc;
+    }
+}
+
+function storeDataLocally(id, data, type) {
 
     if (data != undefined) {
-       return new course(id,data['Course-Title'],data.Description,data.Category,data['Modules-Numbers']);
+
+
+        switch (type){
+            case 'courses':  return new Course(id,data['Course-Title'],data.Description,data.Category,data['Modules-Numbers']);
+            case 'modules':  return new Module(id, data['Module-ID'], data['Module-Title']);
+            case 'lessons':  return new Lesson(id, data['Lesson-ID', data['Lesson-Title']]);
+            case 'concepts': return new Concept(id, data['Concept-ID'],data['Concept-Text']);
+            case 'skills':   return new Skill(id, data['Skill-ID'], data['Skill-Text']);
+            case 'sceneTypes': return new SceneType(id, data['scene-ID'], data['scene-Desc']);
+                
+        }
+        
+           
        
     }
 
--- data_entry_tool/index.js
+++ data_entry_tool/index.js
@@ -26,23 +26,113 @@ document.addEventListener("DOMContentLoaded", function() {
 
 document.querySelector('#load-btn').addEventListener('click', (e)=> {
 e.preventDefault();
-loadDataFromFireStore();
+//loadDataFromFireStore();
+
+
+// fill courses Info
+fillCourseInfo();
+
 
 });
 
 
-function loadDataFromFireStore(e) {
+function loadDataFromFireStore() {
     
-    Courses = [];
+    // re-initialize = when user clicks on load button while there are already data into objects.
+    Courses = []; Lessons = [];
+    Modules = []; Concepts = [];
+    Skills = []; SceneTypes = [];
+
+
     var docRef = db.collection("courses");
     docRef.get().then(function(querySnapshot) {
         querySnapshot.forEach(function(doc) {
-            // doc.data() is never undefined for query doc snapshots
-            //console.log(doc.id, " => ", doc.data());
-            Courses.unshift(storeDataLocally(doc.id,doc.data()));
+           
+            // get Courses Info
+            Courses.unshift(storeDataLocally(doc.id,doc.data(),'courses'));
 
+            // get Concept Info
+            if (doc.data()['Concepts'])
+                (doc.data()['Concepts']).forEach( function(con) {
+                    Concepts.unshift(storeDataLocally(doc.id,con,'concepts'));     
+                });
+                
+            // get Modules Info
+            if (doc.data()['Modules'])
+                (doc.data()['Modules']).forEach (function(mod) {
+                 Modules.unshift(storeDataLocally(doc.id,mod,'modules'));
+                });
+            
 
+            // get Lessons Info
+            if (doc.data()['Lessons'])
+                (doc.data()['Lessons']).forEach(function(les) {
+                Lessons.unshift(storeDataLocally(doc.id,les,'lessons'));
+            });
+
+            // get Skills Info
+            if (doc.data()['Skills'])
+                (doc.data()['Skills']).forEach(function(sk) {
+                    Skills.unshift(storeDataLocally(doc.id,sk,'skills'));
+                });
+
+            
+            // get Scene Types Info
+            if (doc.data()['Scene-Types'])
+                (doc.data()['Scene-Types']).forEach(function (scType) {
+                    SceneTypes.unshift(storeDataLocally(doc.id, scType, 'sceneTypes'));
+                });
         });
     });
+    
+  
+
+}
+
+function fillCourseInfo(){
+
+    const courseCombo = document.getElementById('id-course-title');
+    const courseTitleOption = document.createElement("option");
+    courseTitleOption.text = " HELLO ";
+    courseCombo.add(courseTitleOption,courseCombo[0]);
+
+    const textAreaCourseDesc = document.getElementById('id-course-description');
+    textAreaCourseDesc.value = "JJJJ";
+
+    const chkBoxCourseType = document.getElementById('id-cat-paid');
+    chkBoxCourseType.checked = true;
+
+
+   // const divConceptsList = document.getElementById('list-concepts');
+    //const x = document.createElement("input");
+    // Assigning the attributes 
+    // to created checkbox 
+   // x.type = "checkbox"; 
+   // x.name = "name"; 
+    //x.value = "value"; 
+    //x.className  = "form-check-input";
+   // x.id = "id"; 
+    // creating label for checkbox 
+    //var label = document.createElement('label'); 
+              
+    // assigning attributes for  
+    // the created label tag  
+    
+   // label.className = "form-check-label";
+    //label.htmlFor = "id"; 
+    // appending the created text to  
+    // the created label tag  
+   // label.appendChild(document.createTextNode('This is the label for checkbox.')); 
+      
+ 
+    
+    
+    
+    // appending the checkbox 
+    // and label to div 
+    //divConceptsList.appendChild(x); 
+    //divConceptsList.appendChild(label); 
+    //divConceptsList.innerHTML = '<label> <input type="checkbox" /> <span>Red</span> </label><label> <input type="checkbox" /> <span>Red</span> </label><br>';
+    //divConceptsList.innerHTML += '<input type="checkbox" id="vehicle1" value="Bike"><label for="vehicle1"> I have a bike</label><br>';
     
 }
