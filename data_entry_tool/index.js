32449af94a064e4b94bec415e84bc40868f33fb3
TahaMAhmad <eyad_2222@hotmail.com>
2020-08-02 22:36:56 +0400

    Adding the Main Question Section and Media Section

--- data_entry_tool/index.js
+++ data_entry_tool/index.js
@@ -1,144 +1,122 @@
-
-const loggedOutLinks = document.querySelectorAll('.logged-out');
-const loggedInLinks = document.querySelectorAll('.logged-in');
+const loggedOutLinks = document.querySelectorAll(".logged-out");
+const loggedInLinks = document.querySelectorAll(".logged-in");
 //const mainMsg = document.querySelector('#mainMsg');
 
 const setupUI = (user) => {
-    if (user) {
-        loggedInLinks.forEach (item => item.style.display = "block");
-        loggedOutLinks.forEach(item => item.style.display = "none");
-       // mainMsg.style.display = "none";
-    }else {
-        loggedInLinks.forEach (item => item.style.display = "none");
-        loggedOutLinks.forEach(item => item.style.display = "block");
-        //mainMsg.style.display = "block";
-    }
-}
+  if (user) {
+    loggedInLinks.forEach((item) => (item.style.display = "block"));
+    loggedOutLinks.forEach((item) => (item.style.display = "none"));
+    // mainMsg.style.display = "none";
+  } else {
+    loggedInLinks.forEach((item) => (item.style.display = "none"));
+    loggedOutLinks.forEach((item) => (item.style.display = "block"));
+    //mainMsg.style.display = "block";
+  }
+};
 // setup materialize components
-document.addEventListener("DOMContentLoaded", function() {
-
-    var modals = document.querySelectorAll('.modal');
-    M.Modal.init(modals);
-
+document.addEventListener("DOMContentLoaded", function () {
+  var modals = document.querySelectorAll(".modal");
+  M.Modal.init(modals);
 });
 
- // get data from database
+// get data from database
 
-document.querySelector('#load-btn').addEventListener('click', (e)=> {
-e.preventDefault();
-loadDataFromFireStore();
-
-
+document.querySelector("#load-btn").addEventListener("click", (e) => {
+  e.preventDefault();
+  loadDataFromFireStore();
 });
-
 
 function loadDataFromFireStore() {
-    
-    // re-initialize = when user clicks on load button while there are already data into objects.
-    Courses = []; Lessons = [];
-    Modules = []; Concepts = [];
-    Skills = []; SceneTypes = [];
+  // re-initialize = when user clicks on load button while there are already data into objects.
+  Courses = [];
+  Lessons = [];
+  Modules = [];
+  Concepts = [];
+  Skills = [];
+  SceneTypes = [];
 
+  var docRef = db.collection("courses");
+  docRef.get().then(function (querySnapshot) {
+    querySnapshot.forEach(function (doc) {
+      // get Courses Info
+      Courses.unshift(storeDataLocally(doc.id, doc.data(), "courses"));
 
-    var docRef = db.collection("courses");
-    docRef.get().then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-           
-            // get Courses Info
-            Courses.unshift(storeDataLocally(doc.id,doc.data(),'courses'));
-
-            // get Concept Info
-            if (doc.data()['Concepts'])
-                (doc.data()['Concepts']).forEach( function(con) {
-                    Concepts.unshift(storeDataLocally(doc.id,con,'concepts'));     
-                });
-                
-            // get Modules Info
-            if (doc.data()['Modules'])
-                (doc.data()['Modules']).forEach (function(mod) {
-                 Modules.unshift(storeDataLocally(doc.id,mod,'modules'));
-                });
-            
-
-            // get Lessons Info
-            if (doc.data()['Lessons'])
-                (doc.data()['Lessons']).forEach(function(les) {
-                Lessons.unshift(storeDataLocally(doc.id,les,'lessons'));
-            });
-
-            // get Skills Info
-            if (doc.data()['Skills'])
-                (doc.data()['Skills']).forEach(function(sk) {
-                    Skills.unshift(storeDataLocally(doc.id,sk,'skills'));
-                });
-
-            
-            // get Scene Types Info
-            if (doc.data()['Scene-Types'])
-                (doc.data()['Scene-Types']).forEach(function (scType) {
-                    SceneTypes.unshift(storeDataLocally(doc.id, scType, 'sceneTypes'));
-                });
+      // get Concept Info
+      if (doc.data()["Concepts"])
+        doc.data()["Concepts"].forEach(function (con) {
+          Concepts.unshift(storeDataLocally(doc.id, con, "concepts"));
         });
 
-        
-        // fill courses Info
-        fillCourseInfo();
+      // get Modules Info
+      if (doc.data()["Modules"])
+        doc.data()["Modules"].forEach(function (mod) {
+          Modules.unshift(storeDataLocally(doc.id, mod, "modules"));
+        });
 
-        console.log(Concepts.length);
-        fillConcepts(Concepts);
+      // get Lessons Info
+      if (doc.data()["Lessons"])
+        doc.data()["Lessons"].forEach(function (les) {
+          Lessons.unshift(storeDataLocally(doc.id, les, "lessons"));
+        });
 
+      // get Skills Info
+      if (doc.data()["Skills"])
+        doc.data()["Skills"].forEach(function (sk) {
+          Skills.unshift(storeDataLocally(doc.id, sk, "skills"));
+        });
+
+      // get Scene Types Info
+      if (doc.data()["Scene-Types"])
+        doc.data()["Scene-Types"].forEach(function (scType) {
+          SceneTypes.unshift(storeDataLocally(doc.id, scType, "sceneTypes"));
+        });
     });
-    
-  
 
+    // fill courses Info
+    fillCourseInfo();
+
+    console.log(Concepts.length);
+    fillConcepts(Concepts);
+  });
 }
 
-function fillCourseInfo(){
+function fillCourseInfo() {
+  const courseCombo = document.getElementById("id-course-title");
+  const courseTitleOption = document.createElement("option");
+  courseTitleOption.text = " مرحبا ";
+  courseCombo.add(courseTitleOption, courseCombo[0]);
 
-    const courseCombo = document.getElementById('id-course-title');
-    const courseTitleOption = document.createElement("option");
-    courseTitleOption.text = " مرحبا ";
-    courseCombo.add(courseTitleOption,courseCombo[0]);
+  const textAreaCourseDesc = document.getElementById("id-course-description");
+  textAreaCourseDesc.value = "السلام عليكم ورحمة الله وبركاته";
 
-    const textAreaCourseDesc = document.getElementById('id-course-description');
-    textAreaCourseDesc.value = "السلام عليكم ورحمة الله وبركاته";
+  const chkBoxCourseType = document.getElementById("id-cat-paid");
+  chkBoxCourseType.checked = true;
 
-    const chkBoxCourseType = document.getElementById('id-cat-paid');
-    chkBoxCourseType.checked = true;
+  // const divConceptsList = document.getElementById('list-concepts');
+  //const x = document.createElement("input");
+  // Assigning the attributes
+  // to created checkbox
+  // x.type = "checkbox";
+  // x.name = "name";
+  //x.value = "value";
+  //x.className  = "form-check-input";
+  // x.id = "id";
+  // creating label for checkbox
+  //var label = document.createElement('label');
 
+  // assigning attributes for
+  // the created label tag
 
-   // const divConceptsList = document.getElementById('list-concepts');
-    //const x = document.createElement("input");
-    // Assigning the attributes 
-    // to created checkbox 
-   // x.type = "checkbox"; 
-   // x.name = "name"; 
-    //x.value = "value"; 
-    //x.className  = "form-check-input";
-   // x.id = "id"; 
-    // creating label for checkbox 
-    //var label = document.createElement('label'); 
-              
-    // assigning attributes for  
-    // the created label tag  
-    
-   // label.className = "form-check-label";
-    //label.htmlFor = "id"; 
-    // appending the created text to  
-    // the created label tag  
-   // label.appendChild(document.createTextNode('This is the label for checkbox.')); 
-      
- 
-    
-    
-    
-    // appending the checkbox 
-    // and label to div 
-    //divConceptsList.appendChild(x); 
-    //divConceptsList.appendChild(label); 
-    //divConceptsList.innerHTML = '<label> <input type="checkbox" /> <span>Red</span> </label><label> <input type="checkbox" /> <span>Red</span> </label><br>';
-    //divConceptsList.innerHTML += '<input type="checkbox" id="vehicle1" value="Bike"><label for="vehicle1"> I have a bike</label><br>';
-    
+  // label.className = "form-check-label";
+  //label.htmlFor = "id";
+  // appending the created text to
+  // the created label tag
+  // label.appendChild(document.createTextNode('This is the label for checkbox.'));
+
+  // appending the checkbox
+  // and label to div
+  //divConceptsList.appendChild(x);
+  //divConceptsList.appendChild(label);
+  //divConceptsList.innerHTML = '<label> <input type="checkbox" /> <span>Red</span> </label><label> <input type="checkbox" /> <span>Red</span> </label><br>';
+  //divConceptsList.innerHTML += '<input type="checkbox" id="vehicle1" value="Bike"><label for="vehicle1"> I have a bike</label><br>';
 }
-
-
--- data_entry_tool/script.js
+++ data_entry_tool/script.js
@@ -2,35 +2,39 @@
 
 /* ############################### Hidden Tab Section ################################### */
 
+// click on scene type
 $(".select-scene-click").click(function () {
   var fired_button = $(this).val();
+  // console.log(this);
+  var scenetype = this.textContent;
+  // console.log(scenetype);
   // console.log(fired_button);
-
+  document.getElementById("id-scene-selected").innerHTML = scenetype;
   switch (Number(fired_button)) {
     case 1:
       // console.log(fired_button);
 
-      openQuestionType("Q-type-1");
+      openSceneType("S-type-1");
       break;
 
     case 2:
       // console.log(fired_button);
 
-      openQuestionType("Q-type-2");
+      openSceneType("S-type-2");
       break;
     case 3:
       // console.log(fired_button);
 
-      openQuestionType("Q-type-3");
+      openSceneType("S-type-3");
       break;
     default:
-      openQuestionType("Q-type-1");
+      openSceneType("S-type-1");
       break;
   }
 });
 
-function openQuestionType(scenetype) {
-  // console.log("entered openQuestionType" + scenetype);
+function openSceneType(scenetype) {
+  // console.log("entered openSceneType" + scenetype);
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -42,6 +46,61 @@ function openQuestionType(scenetype) {
   }
   document.getElementById(scenetype).style.display = "block";
 }
+
+//Media General - click on Picture
+$("#id-main-pic-label").click(function () {
+  // console.log("entered function checkbox click");
+
+  if ($("#id-main-pic-checkbox").is(":checked")) {
+    document.getElementById("div-pic-hidden").style.display = "block";
+  } else {
+    document.getElementById("div-pic-hidden").style.display = "none";
+  }
+});
+
+//Media General - click on Recorded Sound
+$("#id-main-sound-label").click(function () {
+  // console.log("entered function checkbox click");
+
+  if ($("#id-main-sound-checkbox").is(":checked")) {
+    document.getElementById("div-sound-hidden").style.display = "block";
+  } else {
+    document.getElementById("div-sound-hidden").style.display = "none";
+  }
+});
+
+//Media General - click on Sound effect
+$("#id-main-soundeffect-label").click(function () {
+  // console.log("entered function checkbox click");
+
+  if ($("#id-main-soundeffect-checkbox").is(":checked")) {
+    document.getElementById("div-soundeffect-hidden").style.display = "block";
+  } else {
+    document.getElementById("div-soundeffect-hidden").style.display = "none";
+  }
+});
+
+//Media General - click on vdieo
+$("#id-main-video-label").click(function () {
+  // console.log("entered function checkbox click");
+
+  if ($("#id-main-video-checkbox").is(":checked")) {
+    document.getElementById("div-video-hidden").style.display = "block";
+  } else {
+    document.getElementById("div-video-hidden").style.display = "none";
+  }
+});
+
+//Media General - click on text
+$("#id-main-text-label").click(function () {
+  // console.log("entered function checkbox click");
+
+  if ($("#id-main-text-checkbox").is(":checked")) {
+    document.getElementById("div-text-hidden").style.display = "block";
+  } else {
+    document.getElementById("div-text-hidden").style.display = "none";
+  }
+});
 
 /* ############################### QTHint Section ################################### */
 /* ############################### Media Section ################################### */
