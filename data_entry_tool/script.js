70fa83d8caee9a6ac2f16272d5f3c8a6690af05d
TahaMAhmad <eyad_2222@hotmail.com>
2020-08-25 21:19:11 +0400

    Changes on the Code - Showing Scene details

--- data_entry_tool/script.js
+++ data_entry_tool/script.js
@@ -4,38 +4,23 @@
 
 // click on scene type
 $(".select-scene-click").click(function () {
-  var fired_button = $(this).val();
+  let sceneTypeNumber = $(this).val();
   // console.log(this);
-  var scenetype = this.textContent;
+  let scenetype = this.textContent;
+
   // console.log(scenetype);
-  // console.log(fired_button);
+  // console.log(sceneTypeNumber);
+
   document.getElementById("id-scene-selected").innerHTML = scenetype;
-  switch (Number(fired_button)) {
-    case 1:
-      // console.log(fired_button);
 
-      openSceneType("S-type-1");
-      break;
-
-    case 2:
-      // console.log(fired_button);
-
-      openSceneType("S-type-2");
-      break;
-    case 3:
-      // console.log(fired_button);
-
-      openSceneType("S-type-3");
-      break;
-    default:
-      openSceneType("S-type-1");
-      break;
-  }
+  openSceneType(scenetype, sceneTypeNumber);
 });
 
-function openSceneType(scenetype) {
-  // console.log("entered openSceneType" + scenetype);
-  var i, tabcontent, tablinks;
+function openSceneType(scenetype, sceneTypeNumber) {
+  // console.log("entered openSceneType " + scenetype);
+  let idScene = "S-type-" + sceneTypeNumber;
+  // console.log(idScene);
+  let i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
@@ -44,7 +29,28 @@ function openSceneType(scenetype) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(scenetype).style.display = "block";
+
+  const selectedSceneTab = document.getElementById(idScene);
+  if (sceneTypeNumber != 3) {
+    while (selectedSceneTab.firstChild) {
+      selectedSceneTab.removeChild(selectedSceneTab.lastChild);
+    }
+  }
+
+  const sceneTitle = document.createTextNode("المشهد: " + scenetype);
+  const QuestionSection = document.createElement("Section");
+
+  QuestionSection.classList.add("genSection");
+
+  selectedSceneTab.appendChild(sceneTitle);
+  selectedSceneTab.appendChild(QuestionSection);
+  /////////////////////////////////////////////
+
+  ////////////////////////////////////////////
+
+  ////////////////////////////////////////////
+
+  selectedSceneTab.style.display = "block";
 }
 
 //Media General - click on Picture
@@ -106,7 +112,7 @@ $("#id-main-text-label").click(function () {
 $("#id-insert-empty").click(function () {
   // console.log("entered function checkbox click");
   console.log("Hello");
-  var contentTxt = document.getElementById("id-statement").value;
+  let contentTxt = document.getElementById("id-statement").value;
   console.log(contentTxt);
   document.getElementById("id-statement").value = contentTxt + " E ";
 });
@@ -114,4 +120,44 @@ $("#id-insert-empty").click(function () {
 /* ############################### QTHint Section ################################### */
 /* ############################### Media Section ################################### */
 /* ############################### Statement & Answer Section ################################### */
+
+// $("#div-multiChoices-s1-textDiv-answer1").slideUp();
+// $("#div-multiChoices-s1-recordedSoundDiv-answer1").slideUp();
+// $("#div-multiChoices-s1-picDiv-answer1").slideUp();
+$(".itemSlide").slideUp();
+
+createSlide(
+  "#id-multiChoices-s1-textRadio-answer1",
+  "#div-multiChoices-s1-textDiv-answer1",
+  "click"
+);
+
+createSlide(
+  "#id-multiChoices-s1-recordedSoundRadio-answer1",
+  "#div-multiChoices-s1-recordedSoundDiv-answer1",
+  "click"
+);
+
+createSlide(
+  "#id-multiChoices-s1-picRadio-answer1",
+  "#div-multiChoices-s1-picDiv-answer1",
+  "click"
+);
+
+function createSlide(ItemEvent, ItemSlide, Eventname) {
+  $(ItemEvent).on(Eventname, function () {
+    $(ItemSlide).slideToggle();
+  });
+}
+
+// console.log($(".itemEventSlide").siblings("div"));
+// createSlide();
+
+// function createSlide() {
+//   $(".itemEventSlide").on("click", function () {
+//     console.log(this.siblings("div"));
+//     $(".itemSlide").slideToggle();
+//   });
+// }
+
 /* ############################### Footer Section ################################### */
